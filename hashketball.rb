e5eb7d147e0bdac3bc4d220881743ce8b549f3a1
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-22 14:43:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,20 @@
 def game_hash
-  
+  game_analysis_hash = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [{
+        :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => 
+      }]
+    },
+    :away => 
+  }
 end
